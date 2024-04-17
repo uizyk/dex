f971e95bb5e1e009773403326c6561adb3424b8a
Sam <uizykim@gmail.com>
2024-04-17 17:51:53 +0900

    dex progress

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="leftH">
+        {/* replace with cointhoth logo */}
+        <h1>LOGO</h1>
+        <Link to="/" className="link">
+          <div className="headerItem">Swap</div>
+        </Link>
+        <Link to="/tokens" className="link">
+          <div className="headerItem">Tokens</div>
+        </Link>
+      </div>
+      <div className="rightH">
+        <div className="headerItem">
+          {/* replace with eth logo */}
+          <h1>ETH</h1>
+        </div>
+        <div className="connectButton">
+          Connect
+        </div>
+      </div>
+    </header>
   )
 }
 
--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Input, Popover, Radio, Modal, message } from "antd";
+import { ArrowDownOutlined, DownOutlined, SettingOutlined } from "@ant-design/icons";
 
 function Swap() {
+  const [slippage, setSlippage] = useState(2.5);
+
+  function handleSlippageChange(e){
+    setSlippage(e.target.value);
+  }
+
+  const settings = (
+    <>
+      <div>Slippage Tolerance</div>
+      <div>
+        <Radio.Group value={slippage} onChange={handleSlippageChange} >
+          <Radio.Button value={0.5}>0.5%</Radio.Button>
+          <Radio.Button value={2.5}>2.5%</Radio.Button>
+          <Radio.Button value={5}>5%</Radio.Button>
+        </Radio.Group>
+      </div>
+    </>
+  )
   return (
-    <div>Swap</div>
+    <div className="tradeBox" >
+      <div className="tradeBoxHeader">
+        <h4>Swap</h4>
+        <Popover
+          content={settings}
+          title="Settings"
+          trigger="click"
+          placement="bottomRight"
+        >
+          <SettingOutlined className="cog" />
+        </Popover>
+      </div>
+    </div>
   )
 }
 
